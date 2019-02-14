58eaea7a206001d5ad136445d3abc4ff53a0c47e
jsuyog2 <jsuyog2@github.com>
2019-02-14 17:23:29 +0530

    change Index Page

--- newQuote/js.php
+++
@@ -1,8 +0,0 @@
-<script src="js/jquery.min.js"></script>
-<script src="js/jquery-1.11.3.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-<script src="https://www.gstatic.com/firebasejs/5.7.2/firebase.js"></script>
-<script src="js/conf.js"></script>
-<script src="js/load.js"></script>
-
-<script src="js/auth.js"></script>
