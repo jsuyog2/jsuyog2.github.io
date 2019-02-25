a2e664c61c36c46f53f06000846a2f1478aad3fc
jsuyog2 <jsuyog2@github.com>
2019-02-25 17:28:32 +0530

    add login

--- js/firebaseScript.js
+++ js/firebaseScript.js
@@ -1,18 +1,86 @@
 var provider = new firebase.auth.GoogleAuthProvider();
 
-//firebase.auth().signInWithPopup(provider).then(function(result) {
-//  // This gives you a Google Access Token. You can use it to access the Google API.
-//  var token = result.credential.accessToken;
-//  // The signed-in user info.
-//  var user = result.user;
-//  // ...
-//}).catch(function(error) {
-//  // Handle Errors here.
-//  var errorCode = error.code;
-//  var errorMessage = error.message;
-//  // The email of the user's account used.
-//  var email = error.email;
-//  // The firebase.auth.AuthCredential type that was used.
-//  var credential = error.credential;
-//  // ...
-//});
+//on Login
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        $('.navbarData').append('<li><a href="dashborad.html">Dashborad</a></li>');
+        $('.navbarData').append(' <li><a onclick="singOut()">Logout</a></li>');
+        if ($('meta[name="login"]').length != 0) {
+
+        }
+    } else {
+        $('.navbarData').append('<li><a href="Login.html">Login</a></li>');
+        if ($('meta[name="login"]').length != 0) {
+            window.location.href = "Login";
+        }
+    }
+});
+
+//google login
+$('#loginGoogle').click(function () {
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+        window.location.href = "index";
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+});
+
+//email password auth
+function login(email, password) {
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function () {
+            window.location.href = "dashborad";
+        })
+        .catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorCode === 'auth/wrong-password') {
+                M.toast({
+                    html: errorCode
+                })
+            } else {
+                M.toast({
+                    html: errorMessage
+                })
+            }
+        });
+}
+
+$("#login").click(function () {
+    var email = $('input[name=username]').val();
+    var password = $('input[name=password]').val();
+    login(email, password);
+});
+
+$(".login").on('keypress', function (e) {
+    if (e.which == 13) {
+        var email = $('input[name=username]').val();
+        var password = $('input[name=password]').val();
+        login(email, password);
+    }
+});
+
+//sign Out
+function singOut() {
+    firebase.auth().signOut().then(function () {
+        M.toast({
+            html: 'Sign-out successful'
+        });
+        sessionStorage.setItem("login", false);
+        window.location.href = "Login";
+    }).catch(function (error) {
+        // An error happened.
+    });
+}
