6b7f152c0b47a1e20414daac2213d1f653e570a1
jsuyog2 <jsuyog2@github.com>
2019-02-28 17:13:36 +0530

    get distance

--- js/script.js
+++ js/script.js
@@ -18,6 +18,10 @@ $(document).ready(function () {
             });
         }
     });
+    
+     $('input.autocomplete').autocomplete({
+      onAutocomplete: function(){getletlong(this)},
+    });
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
