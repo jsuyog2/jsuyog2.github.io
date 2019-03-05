3c3caec711ed17dbccb2d3b6890aadd18e891d13
jsuyog2 <jsuyog2@github.com>
2019-03-05 14:28:01 +0530

    remove google login

--- js/firebaseScript.js
+++ js/firebaseScript.js
@@ -17,27 +17,6 @@ firebase.auth().onAuthStateChanged(function (user) {
     }
 });
 
-//google login
-$('#loginGoogle').click(function () {
-    firebase.auth().signInWithPopup(provider).then(function (result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-        window.location.href = "index";
-    }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-    });
-});
-
 //email password auth
 function login(email, password) {
     firebase.auth().signInWithEmailAndPassword(email, password).then(function () {
@@ -91,7 +70,8 @@ function sendEmailVerification() {
             firebase.database().ref('users/' + user.uid+'/email').set(user.email);
         } else {
             user.sendEmailVerification();
-            window.location.href = "varification";
+            setTimeout(function(){ window.location.href = "varification" }, 3000);
+           
         }
     });
 }
