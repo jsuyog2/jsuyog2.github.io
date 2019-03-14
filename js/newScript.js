3a5fae4e6019a4b396d3bf4a9cabc6c560c3cd9b
jsuyog2 <jsuyog2@github.com>
2019-03-14 12:13:02 +0530

    all fields requried

--- js/newScript.js
+++ js/newScript.js
@@ -89,7 +89,7 @@ function foodCost(breakfast, lunch, dinner, stay) {
 }
 
 function hotelCost(hotelCost, stay, people) {
-    var hotelPrice = (parseInt(hotelCost, 10) * stay) / parseInt(people, 10);
+    var hotelPrice = Math.round((parseInt(hotelCost, 10) * stay) / parseInt(people, 10));
     $('#totalHotel').html(hotelPrice);
     return hotelPrice;
 }
@@ -146,137 +146,7 @@ $(document).ready(function () {
         $(this).html('<img src="https://png.icons8.com/color/' + icon + '">');
     });
 
-    var totalAdults = 0,
-        startingDate = "",
-        endingDate = "",
-        hotel = false,
-        vehicle = false,
-        food = false;
-    $('#mainNextDiv1').click(function () {
-        $('#mainDiv1').slideToggle();
-        $('#mainDiv2').slideToggle();
-    });
-    $('#mainDiv1Back').click(function () {
-        $('#mainDiv1').slideToggle();
-        $('#mainDiv2').slideToggle();
-    });
-    $('#mainNextDiv2').click(function () {
-        startingDate = $('#startingDate').val();
-        endingDate = $('#endingDate').val();
-        if (startingDate !== "" && endingDate !== "") {
-            $('#mainDiv2').slideToggle();
-            $('#mainDiv3').slideToggle();
-        }
-    });
-    $('#mainDiv2Back').click(function () {
-        $('#mainDiv2').slideToggle();
-        $('#mainDiv3').slideToggle();
-    });
-    $('#mainNextDiv3').click(function () {
-        var checkBoxs = [];
-        hotel = $('#hotelEnable').prop("checked");
-        vehicle = $('#vehicleEnable').prop("checked");
-        food = $('#foodEnable').prop("checked");
-        if (hotel === true) {
-            checkBoxs.push('hotel');
-        }
-        if (vehicle === true) {
-            checkBoxs.push('vehicle');
-        }
-        if (food === true) {
-            checkBoxs.push('food');
-        }
-        $('#mainDiv3').slideToggle();
-        addDetails(totalAdults, startingDate, endingDate, checkBoxs);
-    });
 });
-
-
-function addDetails(totalAdults, startingDate, endingDate, checkBoxs) {
-    var i = 0;
-    var perPersonCostPerDay = 0,
-        totalCost = 0,
-        stay = 0,
-        hotelPrice = 0,
-        breakfast = 0,
-        lunch = 0,
-        dinner = 0,
-        foodPrice = 0,
-        car = false,
-        diffVehicle = false,
-        totalBikesCar = 0,
-        fromPlace = '',
-        toPlace = '',
-        km = 0,
-        pdAvg = 0,
-        rate = 0,
-        TicketPrice = 0,
-        returnTicketPrice = 0,
-        totalVehicleCost = 0;
-    $('#' + checkBoxs[i]).slideToggle();
-    days = datediff(startingDate, endingDate);
-
-    if (i === checkBoxs.length - 1) {
-        $('#' + checkBoxs[i] + 'Next').html('Finish');
-        $('#' + checkBoxs[i] + 'Next').addClass('Finish');
-    }
-    $('.next').click(function () {
-        i++;
-        if (this.id === 'hotelNext') {
-            hotelPrice = (parseInt($('#hotelPrice').val(), 10) * stay) / totalAdults;
-        }
-        if (this.id === 'foodNext') {
-            breakfast = parseInt($("#rateBreakfast").val(), 10);
-            lunch = parseInt($("#rateLunch").val(), 10);
-            dinner = parseInt($("#rateDinner").val(), 10);
-            foodPrice = (breakfast + lunch + dinner) * stay;
-        }
-        if (this.id === 'vehicleNext') {
-            car = $('#car').prop("checked");
-            diffVehicle = $('#diffVehicle').prop("checked");
-            var index = checkBoxs.indexOf("vehicle");
-            if (car === true) {
-                checkBoxs.splice(index + 1, 0, "totalCarDiv", "destinationDiv", "carEstimDiv");
-            }
-            if (diffVehicle === true) {
-                checkBoxs.splice(index + 1, 0, "diffVehiDiv");
-            }
-        }
-        if (car === true) {
-            if (this.id === 'totalCarNext') {
-                totalBikesCar = parseInt($("#totalCar").val(), 10);
-            }
-            if (this.id === 'destinationNext') {
-                fromPlace = $('#autocomplete-input1').val();
-                toPlace = $('#autocomplete-input2').val();
-            }
-            if (this.id === 'carEstimDivNext') {
-                km = parseInt($("#km").val(), 10);
-                pdAvg = parseInt($("#pdAvg").val(), 10);
-                rate = parseInt($("#rate").val(), 10);
-            }
-            totalVehicleCost = Math.round((((((km * 2) / pdAvg) * rate) / totalAdults) * totalBikesCar));
-        }
-        if (diffVehicle === true) {
-            if (this.id === 'diffVehiDivNext') {
-                TicketPrice = parseInt($("#ticketPrice").val(), 10);
-                returnTicketPrice = parseInt($("#returnTicketPrice").val(), 10);
-                totalVehicleCost = Math.round(TicketPrice + returnTicketPrice);
-            }
-        }
-        perPersonCostPerDay = hotelPrice + foodPrice + totalVehicleCost;
-        if (i < checkBoxs.length) {
-            $('#' + checkBoxs[i - 1]).slideToggle();
-            $('#' + checkBoxs[i]).slideToggle();
-        }
-        if (i === checkBoxs.length - 1) {
-            $('#' + checkBoxs[i] + 'Next').html('Finish');
-            $('#' + checkBoxs[i] + 'Next').addClass('Finish');
-        }
-    });
-
-}
-
 var divIds = ["userDiv", "daysDiv", "extraDiv"];
 $('.switcher').change(function () {
     if ($(this).prop("checked") === true) {
@@ -323,7 +193,42 @@ $('.vehicleType').change(function () {
 
 $('.addFinish').click(function () {
     $('#' + divIds[divIds.length - 1] + ' .btn').html('Finish');
-    $('#' + divIds[divIds.length - 1] + ' .btn').attr('onclick', 'finish(this)');
+});
+
+$('#hotelDiv input').keyup(function () {
+    if ($(this).val()) {
+        if (divIds.indexOf('hotelDiv') != divIds.length - 1) {
+            $('#hotelDiv .next').attr('onclick', 'toggleNextDiv(this)');
+        } else {
+            $('#hotelDiv .next').attr('onclick', 'finish(this)');
+        }
+    } else {
+        $('#hotelDiv .next').attr('onclick', '');
+    }
+});
+
+$('#foodDiv input').keyup(function () {
+    if ($('#rateBreakfast').val() && $('#rateLunch').val() && $('#rateDinner').val()) {
+        if (divIds.indexOf('foodDiv') != divIds.length - 1) {
+            $('#foodDiv .next').attr('onclick', 'toggleNextDiv(this)');
+        } else {
+            $('#foodDiv .next').attr('onclick', 'finish(this)');
+        }
+    } else {
+        $('#foodDiv .next').attr('onclick', '');
+    }
+});
+
+$('#vehicleDiv input').keyup(function () {
+    if ($('#rate').val() && $('#km').val() && $('#pdAvg').val()) {
+        if (divIds.indexOf('vehicleDiv') != divIds.length - 1) {
+            $('#vehicleDiv .next').attr('onclick', 'toggleNextDiv(this)');
+        } else {
+            $('#vehicleDiv .next').attr('onclick', 'finish(this)');
+        }
+    } else {
+        $('#vehicleDiv .next').attr('onclick', '');
+    }
 });
 
 function removeArrayElement(array, value) {
