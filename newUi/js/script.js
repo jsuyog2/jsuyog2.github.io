d3e33fda5e2fea4598c4fa0e854c97094a767a42
jsuyog2 <jsuyog2@github.com>
2019-03-26 15:31:13 +0530

    map fix ui

---
+++ newUi/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    "use strict";
+    $('select').formSelect();
+    $('#startingDate').datepicker({
+        format: "mm/dd/yyyy",
+        autoClose: true,
+        minDate: new Date(),
+        onSelect: function (date) {
+            $("#endingDate").prop('disabled', false);
+            $('#endingDate').datepicker({
+                format: "mm/dd/yyyy",
+                autoClose: true,
+                minDate: date,
+                onSelect: function () {
+                    $('#2').attr('onclick', 'toggleNextDiv(this)');
+                }
+            });
+        }
+    });
+
+    $('input.autocomplete').autocomplete({
+        onAutocomplete: function () {
+            getletlong(this)
+        },
+    });
+    //display icon         
+    $(".icon").each(function () {
+        var icon = $(this).html();
+        $(this).html('<img src="https://png.icons8.com/color/' + icon + '">');
+    });
+});
