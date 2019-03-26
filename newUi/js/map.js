30bf3310d188dfdb8fb3c33a7e23ff1bf0bebd8f
jsuyog2 <jsuyog2@github.com>
2019-03-26 14:44:41 +0530

    add route

--- newUi/js/map.js
+++ newUi/js/map.js
@@ -1,10 +1,45 @@
+var APPLICATION_ID = 'YLJimxqR6szOHWxWLhex';
+ var APPLICATION_CODE = '8pP47klOkZKCj1XUUmWj4A';
+
 var mapContainer = document.getElementById('map'),
     routeInstructionsContainer = document.getElementById('panel');
 
-var platform = new H.service.Platform({
-    'app_id': 'YLJimxqR6szOHWxWLhex',
-    'app_code': '8pP47klOkZKCj1XUUmWj4A',
-    useHTTPS: true
+ // Initialize the platform object:
+ var platform = new H.service.Platform({
+     'app_id': APPLICATION_ID,
+     'app_code': APPLICATION_CODE
+ });
+
+$(document).ready(function () {
+    "use strict";
+    $('select').formSelect();
+    $('#startingDate').datepicker({
+        format: "mm/dd/yyyy",
+        autoClose: true,
+        minDate: new Date(),
+        onSelect: function (date) {
+            $("#endingDate").prop('disabled', false);
+            $('#endingDate').datepicker({
+                format: "mm/dd/yyyy",
+                autoClose: true,
+                minDate: date,
+                onSelect: function () {
+                    $('#2').attr('onclick', 'toggleNextDiv(this)');
+                }
+            });
+        }
+    });
+
+    $('input.autocomplete').autocomplete({
+        onAutocomplete: function () {
+            getletlong(this)
+        },
+    });
+    //display icon         
+    $(".icon").each(function () {
+        var icon = $(this).html();
+        $(this).html('<img src="https://png.icons8.com/color/' + icon + '">');
+    });
 });
 
 var pixelRatio = window.devicePixelRatio || 1;
@@ -25,8 +60,6 @@ var map = new H.Map(mapContainer,
 
 var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 var ui = H.ui.UI.createDefault(map, defaultLayers);
-
-
 
 function calculateRouteFromAtoB(platform, startingPoint, endPoint) {
     var router = platform.getRoutingService(),
@@ -51,7 +84,6 @@ function onSuccess(result) {
     var route = result.response.route[0];
     addRouteShapeToMap(route);
     addManueversToMap(route);
-    addSummaryToPanel(route.summary);
 }
 
 function onError(error) {
@@ -76,20 +108,6 @@ function addRouteShapeToMap(route) {
     });
     map.addObject(polyline);
     map.setViewBounds(polyline.getBounds(), true);
-}
-
-function addSummaryToPanel(summary) {
-    var summaryDiv = document.createElement('div'),
-        content = '';
-    content += '<b>Total distance</b>: ' + summary.distance + 'm. <br/>';
-    content += '<b>Travel Time</b>: ' + summary.travelTime.toMMSS() + ' (in current traffic)';
-
-
-    summaryDiv.style.fontSize = 'small';
-    summaryDiv.style.marginLeft = '5%';
-    summaryDiv.style.marginRight = '5%';
-    summaryDiv.innerHTML = content;
-    routeInstructionsContainer.appendChild(summaryDiv);
 }
 
 function addManueversToMap(route) {
@@ -125,4 +143,124 @@ Number.prototype.toMMSS = function () {
     return Math.floor(this / 60) + ' minutes ' + (this % 60) + ' seconds.';
 }
 
+ function autoCompleteListener(textBox, event) {
+     if (query != textBox.value) {
+         if (textBox.value.length >= 1) {
+             var params = '?' +
+                 'query=' + encodeURIComponent(textBox.value) + // The search text which is the basis of the query
+                 '&beginHighlight=' + encodeURIComponent('<mark>') + //  Mark the beginning of the match in a token. 
+                 '&endHighlight=' + encodeURIComponent('</mark>') + //  Mark the end of the match in a token. 
+                 '&maxresults=5' + // The upper limit the for number of suggestions to be included 
+                 // in the response.  Default is set to 5.
+                 '&app_id=' + APPLICATION_ID +
+                 '&app_code=' + APPLICATION_CODE;
+             ajaxRequest.open('GET', AUTOCOMPLETION_URL + params);
+             ajaxRequest.send();
+         }
+     }
+     query = textBox.value;
+ }
+
+ function onAutoCompleteSuccess() {
+
+     clearOldSuggestions();
+     addSuggestionsToPanel(this.response); // In this context, 'this' means the XMLHttpRequest itself.
+     addSuggestionsToMap(this.response);
+ }
+
+
+ function onAutoCompleteFailed() {
+     alert('Ooops!');
+ }
+
+ $(".autocomplete").keyup(function () {
+     autoCompleteProduct(this, APPLICATION_ID, APPLICATION_CODE);
+ });
+
+ //get data for autocomplete
+ function autoCompleteProduct(context, APPLICATION_ID, APPLICATION_CODE) {
+     $.ajax({
+         url: "https://autocomplete.geocoder.api.here.com/6.2/suggest.json",
+         dataType: 'json',
+         type: "GET",
+         data: {
+             "query": $(context).val(),
+             "maxresults": 5,
+             "app_id": APPLICATION_ID,
+             "app_code": APPLICATION_CODE
+         },
+         success: function (result) {
+             if ($(context).val() != '') {
+                 autoCompleteSuccess(result, context);
+             }
+         }
+     });
+ }
+
+ //set data into autocomplete
+ function autoCompleteSuccess(response, elem) {
+     var dataLength = Object.keys(response.suggestions).length;
+     var dataArr = {};
+     for (var i = 0; i < dataLength; i++) {
+         //         dataArr.push(response.suggestions[i].label);
+         dataArr[response.suggestions[i].label] = null;
+     }
+     const autocomplete = document.querySelector('#' + elem.id);
+     var instance = M.Autocomplete.getInstance(autocomplete);
+     instance.updateData(dataArr);
+
+ }
+ var startLat, startLong, endLat, endLong;
+
+ function getletlong(context) {
+     $.ajax({
+         url: "https://geocoder.api.here.com/6.2/geocode.json",
+         dataType: 'json',
+         type: "GET",
+         data: {
+             "searchtext": $(context.el).val(),
+             "app_id": APPLICATION_ID,
+             "app_code": APPLICATION_CODE
+         },
+         success: function (result) {
+
+             if (context.el.id == "autocomplete-input1") {
+                 startLat = result.Response.View[0].Result[0].Location.DisplayPosition.Latitude;
+                 startLong = result.Response.View[0].Result[0].Location.DisplayPosition.Longitude;
+                  $("#autocomplete-input2").prop('disabled', false);
+             } else if (context.el.id == "autocomplete-input2") {
+                 endLat = result.Response.View[0].Result[0].Location.DisplayPosition.Latitude;
+                 endLong = result.Response.View[0].Result[0].Location.DisplayPosition.Longitude;
+                 getDistance(startLat, startLong, endLat, endLong);
+                 calculateRouteFromAtoB(platform,startLat+','+startLong,endLat+','+endLong);
+             }
+         }
+     });
+ }
+
+ function getDistance(startLat, startLong, endLat, endLong) {
+     var startLatLong = startLat + ',' + startLong;
+     var endLatLong = endLat + ',' + endLong;
+     $.ajax({
+         url: "https://route.api.here.com/routing/7.2/calculateroute.json",
+         dataType: 'json',
+         type: "GET",
+         data: {
+             "waypoint0": startLatLong,
+             "waypoint1": endLatLong,
+             "mode": 'fastest;car;traffic:enabled',
+             "app_id": APPLICATION_ID,
+             "app_code": APPLICATION_CODE
+         },
+         success: function (result) {
+             var temp = result.response.route[0].summary.distance
+             var one = parseInt(String(temp).charAt(0));
+             var numDec = (String(temp/1000).split(".")[0].length)*10;
+             var extraKM = one*numDec;
+             var distance = Math.round((result.response.route[0].summary.distance / 1000));
+             $("#km").val(distance);
+             M.updateTextFields();
+         }
+     });
+ }
 //        calculateRouteFromAtoB(platform,,);
