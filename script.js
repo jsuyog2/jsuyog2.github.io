f0ab4b56ac7e7f8b70e8f2d419ba4201debeed39
Suyog Jadhav <jsuyog2@gmail.com>
2020-04-12 16:24:13 +0530

    remove end part code

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ $.ajax({
         client_secret: client_secret,
         grant_type: "authorization_code",
         redirect_uri: redirect_uri,
-        code: getUrlVars()["code"]
+        code: getUrlVars()["code"].slice(0, -2); 
     },
     success: function (response) {
         console.log(response)
@@ -33,11 +33,3 @@ $.ajax({
 
 
 console.log("curl -X POST https://api.instagram.com/oauth/access_token -F client_id=" + appid + " -F client_secret=" + client_secret + " -F grant_type=authorization_code -F redirect_uri=" + redirect_uri + " -F code=" + getUrlVars()["code"]);
-
-"curl -X POST"
-"https://api.instagram.com/oauth/access_token"
-"-F client_id=491767988111690"
-"-F client_secret=41e83aee7b9f0a25983a3a9685f8e3c8"
-"-F grant_type=authorization_code"
-"-F redirect_uri=https://jsuyog2.github.io/InstagramInsights"
-"-F code=AQDjnH9lUzQHSCb8I7E97p6v2rUosxD44m63mng2tbUiQ225r8KyTdOjDsXKq3YlBbZCPnayoA8uhKG7bqFqDATQ93_H3zRo_aqdO3uzILGV0VUHpU_u0eLvqk03DUiYBpDl6ee9DvLy4kG0_2ehjhTUUpURA_yjixaqD-eXI16xJHnODPbdJO1vuMC2zg3frh-5rfiFikHTUPoc0kfzUOGRzlbojHueaND7-B4BM44sZQ#_"
