95fec5a988e1e1d4ece43fc6050e7d79f7cfefbf
Suyog Jadhav <jsuyog2@gmail.com>
2020-04-12 15:38:24 +0530

    add method

--- InstagramInsights/script.js
+++ InstagramInsights/script.js
@@ -1,18 +1,6 @@
-// $.ajax({
-//     url: 'https://api.instagram.com/oauth/authorize',
-//     async: false,
-//     data: {
-//         client_id: "491767988111690",
-//         redirect_uri: "https://jsuyog2.github.io/",
-//         scope: "user_profile,user_media",
-//         response_type: "code"
-//     },
-//     success: function (response) {
-//         console.log(response)
-//     }
-// });
 var appid = "491767988111690";
 var redirect_uri = "https://jsuyog2.github.io/InstagramInsights";
+var client_secret = "41e83aee7b9f0a25983a3a9685f8e3c8";
 
 function getUrlVars() {
     var vars = {};
@@ -26,3 +14,19 @@ if (getUrlVars()["code"] === undefined) {
     var url = "https://api.instagram.com/oauth/authorize?client_id=" + appid + "&redirect_uri=" + redirect_uri + "&scope=user_profile,user_media&response_type=code"
     window.location.href = url;
 }
+
+$.ajax({
+    url: 'https://api.instagram.com/oauth/access_token',
+    type:"POST",
+    async: false,
+    data: {
+        client_id: appid,
+        client_secret: client_secret,
+        grant_type: "authorization_code",
+        redirect_uri: redirect_uri,
+        code: getUrlVars()["code"]
+    },
+    success: function (response) {
+        console.log(response)
+    }
+});
