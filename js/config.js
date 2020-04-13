76b6c55bad85802af9488fbc6b8e2f063bccbfac
Suyog Jadhav <jsuyog2@gmail.com>
2020-04-13 14:43:02 +0530

    remove local

--- js/config.js
+++ js/config.js
@@ -3,8 +3,6 @@ var redirect_uri = "https://jsuyog2.github.io/";
 var client_secret = "41e83aee7b9f0a25983a3a9685f8e3c8";
 var access_token = localStorage.getItem("InstaAceessToken");
 var graphAccessToken = localStorage.getItem("FBAceessToken");
-localStorage.removeItem("InstaAceessToken");
-localStorage.removeItem("FBAceessToken");
 var ApiUrl = "https://graph.instagram.com/me";
 var graphUrl = "https://graph.facebook.com/v6.0/";
 var FBInit = {
