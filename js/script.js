0e8ec817e1283399aeb9b8a40794234a8bd6897f
Suyog Jadhav <jsuyog2@gmail.com>
2020-04-13 15:08:18 +0530

    change header

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ if (access_token !== null && graphAccessToken !== null) {
         url: graphUrl + userId + "?fields=" + fields.join() + "&access_token=" + graphAccessToken,
         async: false,
         success: function (response) {
-            $("h3").html(response.name);
+            $(".header").html(response.name);
             $("h5").html(response.biography);
             $("h6").html(response.website);
             $("#profile_pic").html("<img src='" + response.profile_picture_url + "'/>");
