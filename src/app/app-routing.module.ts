d3c3838a826e3cc14ee11bf499ff41f380a6155d
Suyog Jadhav <jsuyog2@gmail.com>
2023-03-08 16:20:08 +0530

    inital commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './component/home/home.component';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [{ path: '', component: HomeComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+declare var $: any;
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'Suyog Jadhav';
+}
